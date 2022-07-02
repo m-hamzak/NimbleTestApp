62b1b16c2b9d7e9ba3c3288def0b65aaad60e2cf
m-hamzak <m_hamzak1997@yahoo.com>
2022-07-03 00:03:10 +0500

    HomeView Xib designed

---
+++ NimbleTestApp/UI/Views/Xibs/SurveyView/SurveyView.swift
@@ -0,0 +1,20 @@
+//
+//  SurveyView.swift
+//  NimbleTestApp
+//
+//  Created by Hamza Khalid on 02/07/2022.
+//
+
+import UIKit
+
+class SurveyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
